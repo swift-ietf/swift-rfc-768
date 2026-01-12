1c16b6c26e7da21ec4118f996fad61be235f1f7a
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-01-12 20:15:13 +0100

    Convert dependencies to remote URLs

--- Package.swift
+++ Package.swift
@@ -24,9 +24,9 @@ let package = Package(
         .library(name: .rfc768, targets: [.rfc768])
     ],
     dependencies: [
-        .package(path: "../../swift-primitives/swift-standard-library-extensions"),
-        .package(path: "../../swift-foundations/swift-ascii"),
-        .package(path: "../swift-rfc-791"),
+        .package(url: "https://github.com/swift-primitives/swift-standard-library-extensions.git", from: "0.0.1"),
+        .package(url: "https://github.com/swift-foundations/swift-ascii.git", from: "0.0.1"),
+        .package(url: "https://github.com/swift-standards/swift-rfc-791.git", from: "0.0.1"),
     ],
     targets: [
         .target(
